02124a9640eee58f1521657852cc4443fa7dc831
umeshmaurya805 <63446830+umeshmaurya805@users.noreply.github.com>
2020-04-24 20:30:04 +0530

    Updates

---
+++ precache-manifest.37aba8da1d606c9d0324ba99d4744c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac49c85264c68e71a3cbc713e168e68a",
+    "url": "/navbar/index.html"
+  },
+  {
+    "revision": "5ce9b3cecc927f449e58",
+    "url": "/navbar/static/css/main.0230f0f2.chunk.css"
+  },
+  {
+    "revision": "5c3b256c91dfe1b832c0",
+    "url": "/navbar/static/js/2.0fea23ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/navbar/static/js/2.0fea23ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce9b3cecc927f449e58",
+    "url": "/navbar/static/js/main.ed80759d.chunk.js"
+  },
+  {
+    "revision": "16315120e25c8dbaec0c",
+    "url": "/navbar/static/js/runtime-main.4ed3bbab.js"
+  }
+]);
